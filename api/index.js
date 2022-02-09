dc34252bbe7e5eb32e49f2a128800644f37e39d8
Rohit <prjagtap90@gmail.com>
2022-02-09 16:38:48 +0530

    NETFLIX

---
+++ api/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const mongoose = require("mongoose")
+const dotenv = require('dotenv');
+const authRoute = require("./routes/auth")
+const userRoute = require("./routes/users")
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    // useCreateIndex: true,
+  })
+  .then(() => console.log("DB Connection Successfull"))
+  .catch((err) => {
+    console.error(err);
+  });
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute);
+
+
+app.listen(process.env.PORT, () => {
+  console.log("Backend server is running!");
+});
